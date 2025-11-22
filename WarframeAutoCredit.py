90c488fb6c4add41ac803be875051e49e6880ca8
Ata Zakeria <162620573+Ata-Z@users.noreply.github.com>
2025-11-22 20:33:31 +0000

    Update WarframeAutoCredit.py
    Refactor macro logic for improved clarity and prevent phantom presses.

--- WarframeAutoCredit.py
+++ WarframeAutoCredit.py
@@ -4,12 +4,11 @@ import random
 import time
 import threading
 
-# Global variable to control the macro's state
 macro_running = False
 
 # Coordinates and color for the operator form check
 x, y = 1855, 958  # Screen coordinates
-operator_color = (230, 229, 229)  # RGB color in operator form
+operator_color = (230, 229, 229)  # RGB color in operator form of the screen element I want
 
 def check_operator_form():
     # Check if the color at the specified coordinates matches the operator color.
@@ -17,13 +16,12 @@ def check_operator_form():
     return pixel_color == operator_color
 
 def run_macro():
-    # Run the main macro loop.
     while macro_running:
         # Press '5' to switch to operator form
         keyboard.press('5')
         time.sleep(random.uniform(0.05, 0.1))
         keyboard.release('5')
-        time.sleep(random.uniform(0.5, 0.7))  # Small delay before pressing '5' again
+        time.sleep(random.uniform(0.5, 0.7))  # Small delay before pressing '5' again to prevent phantom presses
 
         # Press '5' again to switch back to Umbra form
         keyboard.press('5')
@@ -35,18 +33,17 @@ def run_macro():
         keyboard.release('a')
 
 
-        # Wait for a random time before restarting the loop
+        # Wait for a random time before restarting the loop to prevent AFK kicks
         time.sleep(random.uniform(3, 5))
 
         # Check if we are still in operator form at the end of the loop
         if not check_operator_form():
-            print("Not in operator form! Pressing '5' to correct...")
-            keyboard.press('5')  # Press '5' again to correct
+            print("Not in operator form! Pressing '5' to correct")
+            keyboard.press('5')
             time.sleep(random.uniform(0.05, 0.1))
             keyboard.release('5')
 
 def toggle_macro():
-    # Toggle the macro on or off
     global macro_running
     if macro_running:
         # Stop the macro
